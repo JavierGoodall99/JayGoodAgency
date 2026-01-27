57099c23fca707936dffec73cfad21e8d46a7a5a
Javier Goodall <114731785+JavierGoodall99@users.noreply.github.com>
2026-01-27 14:29:36 +0200

    refactor: Remove BrandAlchemist component and related services; update types

--- types.ts
+++ types.ts
@@ -13,9 +13,4 @@ export interface ProjectItem {
   year: string;
 }
 
-export interface BrandConcept {
-  tagline: string;
-  manifesto: string;
-  visualDirection: string;
-  colorPalette: string[];
-}
+
